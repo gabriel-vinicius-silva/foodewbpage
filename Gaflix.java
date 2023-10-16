16f4d0a9751a610c36b1861ee46747301c5f15bd
Gabriel <Gabriel@DESKTOP-3VLQ4KN>
2023-10-16 14:43:57 -0300

    :heavy_plus_sign: feat: começando um progeto java  Ref.: hackToberFast2023

---
+++ Gaflix.java
@@ -0,0 +1,37 @@
+
+public class Gaflix {
+    public static void main (String[] args) {
+        //variaveis 
+    				//variveis int
+                    int anoFilme1 = 2017;
+                    int anoFilme2 = 2023;
+                   //variveis na forma String
+                    String  filmes3 = "pregando o amor";
+
+         // Mensagem de boas-vindas
+        System.out.println("bem-vindo a GabriFlix");
+
+
+         // Selecione um filme ou série
+        System.out.println("Selecione o  filme/serie : ");
+        //primeiro filme
+        System.out.println("Filme : ate o ultimo homem");
+        //segundo filme
+        System.out.println("Serie : the chosen");
+        
+        // Ano de lançamento do filme ou série 
+         
+         //lançamentfilme 2
+        System.out.println("serie lançada em  : " + anoFilme1);
+        System.out.println("filme lançado em   : " + anoFilme2);
+        
+
+        // Notas dos filmes
+        System.out.println("classificação dos filmes");
+        System.out.println("ate o ultimo homem: 5 estrelas");
+        System.out.println("the chosen: 4 estrelas");
+        
+   }
+
+}
+
